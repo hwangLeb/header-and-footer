683224ce14a9fe9df5738fe2f63f8abf0ca01613
Viel Eugenio <137420722+hwangLeb@users.noreply.github.com>
2024-04-30 21:31:45 +0800

    Add files via upload

---
+++ activity/index.php
@@ -0,0 +1,9 @@
+<?php include 'header.html'; ?>
+
+<section class="main-content">
+    <h3>Find Your Perfect PC, Laptop, Console and many more.</h3>
+    <!-- Add more content here -->
+</section>
+
+<?php include 'footer.html'; ?>
+    
